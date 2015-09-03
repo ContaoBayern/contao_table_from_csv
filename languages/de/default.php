11d7a64158a95c1aeb4c092a26f66ebb56d463ca
Andreas Fieger <fiedsch@ja-eh.at>
2015-09-03 14:56:24 +0200

    Create default.php

---
+++ languages/de/default.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * CSV Table Content Element for Contao CMS (Copyright (c) 2005-2015 Leo Feyer)
+ *
+ * Copyright (c) 2015 Andreas Fieger
+ *
+ * @license LGPL-3.0+
+ */
+
+
+
+$GLOBALS['TL_LANG']['CTE']['csvtable'] = array(
+			      'Tabelle aus CSV Datei',
+			      'Frontend-Tabelle aus CSV Datei erstellen',
+			      );
+
